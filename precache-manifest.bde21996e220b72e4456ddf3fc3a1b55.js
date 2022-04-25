76239c26a3db64d085a3df10f6977959d3fa1d98
unknown <affatatiej@gmail.com>
2022-04-24 23:19:17 -0500

    Updates

---
+++ precache-manifest.bde21996e220b72e4456ddf3fc3a1b55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbd22478578e550f99cc",
+    "url": "/react-smooth-scrolling-gh-pages/static/css/main.16d33bac.chunk.css"
+  },
+  {
+    "revision": "fbd22478578e550f99cc",
+    "url": "/react-smooth-scrolling-gh-pages/static/js/main.fbd22478.chunk.js"
+  },
+  {
+    "revision": "15f3b0bf1793bea1186d",
+    "url": "/react-smooth-scrolling-gh-pages/static/js/1.15f3b0bf.chunk.js"
+  },
+  {
+    "revision": "e9cbeb0b2231b4a57da0",
+    "url": "/react-smooth-scrolling-gh-pages/static/js/runtime~main.e9cbeb0b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-smooth-scrolling-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "007e5e02d220a4ce6baf2a0965e89739",
+    "url": "/react-smooth-scrolling-gh-pages/index.html"
+  }
+];
